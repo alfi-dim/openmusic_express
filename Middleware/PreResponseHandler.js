d08407858f6621526a8d69f0bdcfcad9ecd9b18d
Zatfley <alfi.dim1612@gmail.com>
2023-08-09 19:03:24 +0700

    Refactor: Updated error handler value

--- Middleware/PreResponseHandler.js
+++ Middleware/PreResponseHandler.js
@@ -3,14 +3,14 @@ const ClientError = require('../Exceptions/ClientError');
 const PreResponseHandler = (err, req, res, next) => {
   if (err) {
     if (err instanceof ClientError) {
-      return res.status(err.statusCode).json({ success: false, error: err.message });
+      return res.status(err.statusCode).json({ status: 'fail', error: err.message });
     }
 
     if (err.name === 'ValidationError') {
       // eslint-disable-next-line no-underscore-dangle
-      return res.status(400).json({ success: false, error: err._message });
+      return res.status(400).json({ status: 'fail', error: err._message });
     }
-    return res.status(500).json({ success: false, error: 'server failure' });
+    return res.status(500).json({ status: 'fail', error: 'server failure' });
   }
   return next();
 };
