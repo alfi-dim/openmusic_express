337108e7ad1261659c72bf47f25e572c3f5d4402
Zatfley <alfi.dim1612@gmail.com>
2023-08-09 16:43:06 +0700

    Feat: Define use case contract for consistency implementation

---
+++ Interfaces/contracts/useCase.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars,class-methods-use-this */
+class UseCase {
+  async execute(useCasePayload) {
+    throw new Error('Method not yet implemented');
+  }
+
+  verifyPayload(useCasePayload) {
+    throw new Error('Method not yet implemented');
+  }
+}
