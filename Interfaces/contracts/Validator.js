06a495cbc9e5da8b776e5064ac0240af319eca6d
Dimas Alfiansyah <alfi.dim1612@gmail.com>
2023-08-16 21:12:43 +0700

    Feat: created validator contract for validation feature

---
+++ Interfaces/contracts/Validator.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars,class-methods-use-this */
+class ValidatorContract {
+  constructor() {
+    this.rule = {};
+  }
+
+  createRule(name) {
+    throw new Error('Method not yet implemented');
+  }
+
+  initiateRule(name, rule) {
+    throw new Error('Method not yet implemented');
+  }
+}
+
+module.exports = ValidatorContract;
